18ddecd75b370d6ec148c3e1643feb9e32103f78
Ricardo Depena <ricardo@isnull.co>
2021-08-26 17:03:34 -0400

    Added navigate button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Fin = Openfin.Desktop;
+using System.Drawing;
 
 
 namespace WpfApp5
@@ -36,7 +37,10 @@ namespace WpfApp5
             {
                 Version = RuntimeVersion,
                 EnableRemoteDevTools = true,
-                RuntimeConnectTimeout = 20000
+                RuntimeConnectTimeout = 20000,
+                Arguments = "--disable-features=CookiesWithoutSameSiteMustSecure,SameSiteByDefaultCookies",
+                SecurityRealm = Guid.NewGuid().ToString(),
+                RemoteDevToolsPort = 9090,
             };
 
             var fin = Fin.Runtime.GetRuntimeInstance(runtimeOptions);
@@ -48,6 +52,9 @@ namespace WpfApp5
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            var windowOptions = new Openfin.Desktop.WindowOptions(System.Guid.NewGuid().ToString(), "https://install-staging.openfin.co/health/");
+            
+            
             try
             {
                 var win = new OfWindow();
@@ -76,6 +83,35 @@ namespace WpfApp5
             {
 
             }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var win = new OfWindow();
+
+            win.Loaded += (senderr, ee) =>
+            {
+                try
+                {
+
+                    var uuid = System.Guid.NewGuid().ToString();
+                    var application = new Openfin.Desktop.ApplicationOptions(uuid, uuid, "https://install-staging.openfin.co/health/");
+                    win.OpenFinEmbeddedView.Initialize(runtimeOptions, application);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+            };
+
+
+            win.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            OpenFinEmbeddedView.OpenfinWindow.navigate("https://install-staging.openfin.co/health/?q=1");   
         }
     }
 }
